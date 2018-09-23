548936d8086ab55bc78aa5d93e4753bfe4485ef8
Ganesh Kumar Pericherla <ganesh.pericherla@gmail.com>
2018-09-23 14:41:12 -0500

    Added logic to get tokens from CharacterSequence

--- src/main/java/com/ganesh/interview/textParserExcercise/TextTokenizer.java
+++ src/main/java/com/ganesh/interview/textParserExcercise/TextTokenizer.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 import com.ganesh.interview.textToken.TextToken;
+import com.ganesh.interview.tokenUtility.TokenUtility;
 
 public class TextTokenizer implements ITextTokenizer {
 
@@ -11,11 +12,26 @@ public class TextTokenizer implements ITextTokenizer {
 	public List<TextToken> getAllTokens(CharSequence cs) {
 		
 		
+		if(!TokenUtility.isValidCS(cs)) return null;
+		
 		List<TextToken> tokenList=new ArrayList<>();
 		
-		// TODO 
+		int start=0;
+		
+		for(int i=0;i<cs.length();i++) {
+			if(!TokenUtility.isAlfhaNumeric(cs.charAt(i))) {
+				if(cs.subSequence(start, i).length()>0)
+				addTokenToList(tokenList, start,i-1, cs.subSequence(start, i));
+				start= i+1;
+			}
+		}
 		
 		return tokenList;
+	}
+	
+	private void addTokenToList(List<TextToken> tokenList,int start,int end, CharSequence tokenValue) {
+		TextToken token=new TextToken(tokenValue.toString().toCharArray(), start,end);
+		tokenList.add(token);
 	}
 		
 }
---
+++ src/main/java/com/ganesh/interview/tokenUtility/TokenUtility.java
@@ -0,0 +1,19 @@
+package com.ganesh.interview.tokenUtility;
+
+public class TokenUtility {
+	
+	public static boolean isValidCS(CharSequence cs) {
+		if(cs!=null
+			&& cs.length()>0
+			&& cs.toString().replaceAll("[^a-zA-Z0-9]+", "").trim().length()>0) return true;
+		return false;
+	}
+	
+	public static boolean isAlfhaNumeric(char c) {
+		if((c >= '0' && c <= '9') ||(c >= 'a' && c <= 'z') ||(c >= 'A' && c<= 'Z')) {
+			return true;
+		}
+		
+		return false;
+	}
+}
